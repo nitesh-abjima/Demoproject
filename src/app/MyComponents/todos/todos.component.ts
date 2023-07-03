b9b03fb3714ee1dca13ea2928669762589b4d5ce
nitesh-abjima <128471408+nitesh-abjima@users.noreply.github.com>
2023-07-03 18:44:48 +0530

    new commit

---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit{
+
+  todos: Todo[];
+  constructor() {
+    this.todos = [
+      {
+        sno: 1,
+        title: "This is title",
+        desc: "Description",
+        active: true
+
+      },
+      {
+        sno: 2,
+        title: "This is title2",
+        desc: "Description",
+        active: true
+
+      },
+      {
+        sno: 3,
+        title: "This is title3",
+        desc: "Description",
+        active: true
+
+      }
+    ]
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  deleteTodo(todo:Todo){
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1)
+  }
+
+}
